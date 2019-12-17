05724b7bf326b458d2bdc6401f710e4b863762f4
Sangho <sanghapriya.bose@gmail.com>
2019-12-17 23:10:00 +0000

    add bootstrap, implement menu, drag element from menu

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import Square from './components/square.component';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export default class App extends Component{
 
 constructor() {
   super();
-  this.state = { testVar:""};
+  this.state = { elements:[]
+                };
   this.clickMe = this.clickMe.bind(this);
   this.onObjectDragged = this.onObjectDragged.bind(this);
+  this.onObjectDragEnd = this.onObjectDragEnd.bind(this);
+  this.onMenuObjectDragEnd = this.onMenuObjectDragEnd.bind(this);
 
 
 }
@@ -34,12 +39,55 @@ onObjectDragged(){
 }
 
 
+onMenuObjectDragEnd(e){
+
+        this.setState({
+                        elements:
+                          <Square   pos1={-1*e.clientX}
+                                    pos2= {-1*e.clientY}
+                                    pos3={e.clientX}
+                                    pos4={e.clientY}
+                                    top={e.clientY}
+                                    left={e.clientX }/>
+
+        });
+
+
+        console.log(this.state)
+
+
+        }
+
+onObjectDragEnd(e){
+        this.setState({
+                  pos1 :this.state.pos3 - e.clientX,
+                  pos2 :this.state.pos4 - e.clientY,
+                  pos3 :e.clientX,
+                  pos4 :e.clientY,
+                  top: ( this.state.top -(this.state.pos4 - e.clientY)),
+                  left:(this.state.left -(this.state.pos3 - e.clientX)) });
+
+        console.log(this.state)
+
+        }
+
+
 render() {
   return (
-    <div className="App">
-      <Square clickMe={this.clickMe} onObjectDragged = {this.onObjectDragged}/>
-      <h2>{this.state.testVar}</h2>
-    </div>
+    <div>
+    <Dropdown>
+    <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Menu
+    </Dropdown.Toggle>
+
+    <Dropdown.Menu>
+      <Dropdown.Item draggable onDragEnd = {this.onMenuObjectDragEnd}>Square</Dropdown.Item>
+      <Dropdown.Item draggable>Circle</Dropdown.Item>
+      <Dropdown.Item draggable>Diamond</Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
+      {this.state.elements}
+      </div>
   );
 }
 
--- src/components/square.component.js
+++ src/components/square.component.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 
-// const squareDiv = {
-//                   position: "absolute",
-//                   zIndex: 9,
-//                   backgroundColor: "#f1f1f1",
-//                   textAlign: "center",
-//                   border: "1px solid #d3d3d3",
-//                   top:
-//                 };
-
 export default class Square extends Component {
-
-
 
   constructor(props) {
   super(props);
@@ -19,42 +8,22 @@ export default class Square extends Component {
               {
                 clickMe: props.clickMe,
                 onObjectDragged: props.onObjectDragged,
-                pos1:0,
-                pos2:0,
-                pos3:0,
-                pos4:0,
-                top:0,
-                left:0
+                onObjectDragEnd: props.onObjectDragEnd,
+                pos1:props.pos1,
+                pos2:props.pos2,
+                pos3:props.pos3,
+                pos4:props.pos4,
+                top:props.top,
+                left:props.left
 
               };
 
-  this.onObjectDragEnd = this.onObjectDragEnd.bind(this);
 
 
 }
 
 
 componentDidMount() {}
-
-
-onObjectDragEnd(e){
-
-  console.log(e.offsetTop);
-
-this.setState({
-          pos1 :this.state.pos3 - e.clientX,
-          pos2 :this.state.pos4 - e.clientY,
-          pos3 :e.clientX,
-          pos4 :e.clientY,
-          top: ( this.state.top -this.state.pos4 + e.clientY),
-          left:(this.state.left + e.clientY) });
-
-
-
-console.log(this.state)
-
-        }
-
 
 
 
@@ -64,6 +33,7 @@ render() {
   return (
 
     <div style = {{
+
                       position: "absolute",
                       zIndex: 9,
                       backgroundColor: "#f1f1f1",
@@ -71,18 +41,19 @@ render() {
                       border: "1px solid #d3d3d3",
                       top: this.state.top+"px",
                       left: this.state.left+"px"
-                    }} onClick = {this.state.clickMe}
-    onDrag = {this.state.onObjectDragged} onDragEnd = {this.onObjectDragEnd}>
+                    }}
+        draggable
+        onClick = {this.state.clickMe}
+        onDrag = {this.state.onObjectDragged}
+        onDragEnd = {this.state.onObjectDragEnd}>
 
 
-    <div
-
-      style = {{
-        padding: "10px",
-         cursor: "move",
-         zIndex: 10,
-         backgroundColor: "#2196F3",
-         color: "#fff"}}>
+    <div style = {{
+                   padding: "10px",
+                   cursor: "move",
+                   zIndex: 10,
+                   backgroundColor: "#2196F3",
+                   color: "#fff"}}>
 
 
           Hello There!!!
